0e28de230222b7b0a95893d99eaf0c14a464ca04
Keppler515 <maxisosa25@hotmail.com>
2022-03-27 18:31:25 -0300

    Ejercicios I01 a I07

---
+++ EjercicioI07/LogicaRecibos/CalculadorRecibos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*
+Consigna#
+Se debe pedir el ingreso por teclado del valor hora, el nombre, la antigüedad (en años) 
+y la cantidad de horas trabajadas en el mes de N cantidad de empleados de una fábrica.
+
+Se pide calcular el importe a cobrar teniendo en cuenta que el total 
+(que resulta de multiplicar el valor hora por la cantidad de horas trabajadas), 
+hay que sumarle la cantidad de años trabajados multiplicados por $150, y al total de 
+todas esas operaciones restarle el 13% en concepto de descuentos.
+
+Mostrar el recibo correspondiente con el nombre, la antigüedad, 
+el valor hora, el total a cobrar en bruto y el total a cobrar neto 
+de todos los empleados ingresados.
+ */
+
+
+namespace LogicaRecibos
+{
+    public static class CalculadorRecibos
+    {
+        public static float totalNeto;
+        public static float totalBruto;
+        public static float descuento;
+        public static float final;
+
+        public static float CalcularRecibos(float valorHora, string nombre, int antiguedad, float horasTrabajadas)
+        {
+            totalNeto = valorHora * horasTrabajadas;
+            totalBruto = totalNeto + antiguedad * 150;
+            descuento = (totalBruto * 13) / 100;
+            final = totalBruto - descuento;
+            return final;
+        }
+    }
+}
---
+++ EjercicioI07/VistaRecibos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using LogicaRecibos;
+
+namespace VistaRecibos
+{
+    internal class Program
+    {
+        public static float valorHora, horasTrabajadas;
+        public static int antiguedad;
+        public static string nombre;
+        public static string stringValorHora, stringHorasTrabajadas, stringAntiguedad;
+        public static string stringOpcion;
+        public static int opcion = 0;
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Valor hora: ");
+                stringValorHora = Console.ReadLine();
+                float.TryParse(stringValorHora, out valorHora);
+
+                Console.WriteLine("Nombre del empleado: ");
+                nombre = Console.ReadLine();
+
+                Console.WriteLine("Antigüedad: ");
+                stringAntiguedad = Console.ReadLine();
+                int.TryParse(stringAntiguedad, out antiguedad);
+
+                Console.WriteLine("Horas trabajadas: ");
+                stringHorasTrabajadas = Console.ReadLine();
+                float.TryParse(stringHorasTrabajadas, out horasTrabajadas);
+
+                CalculadorRecibos.CalcularRecibos(valorHora, nombre, antiguedad, horasTrabajadas);
+
+                Console.WriteLine($"Empleado: {nombre}");
+                Console.WriteLine($"Antigüedad: {antiguedad}");
+                Console.WriteLine($"Valor hora: {valorHora}");
+                Console.WriteLine($"Total bruto: {CalculadorRecibos.totalBruto}");
+                Console.WriteLine($"El valor final es: {CalculadorRecibos.final}");
+
+                Console.WriteLine("Desea continuar?");
+                Console.WriteLine("1: si");
+                Console.WriteLine("0: no");
+                stringOpcion = Console.ReadLine();
+                int.TryParse(stringOpcion, out opcion);
+
+            } while (opcion != 0);
+        }
+    }
+}

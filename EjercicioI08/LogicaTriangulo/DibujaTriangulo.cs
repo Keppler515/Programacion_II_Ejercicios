a08257d30ead7d1dc9dae63fda5b4b4888c464e1
Keppler515 <maxisosa25@hotmail.com>
2022-03-27 19:03:32 -0300

    Ejercicios I01 a I08

---
+++ EjercicioI08/LogicaTriangulo/DibujaTriangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LogicaTriangulo
+{
+    public static class DibujaTriangulo
+    {
+        public static string asterisco = "*";
+        public static string DibujarTriangulo(int altura)
+        {
+            for (int i = 0; i < altura; i++)
+            {
+                Console.WriteLine($"{asterisco}");
+                asterisco = asterisco + "*";
+            }
+
+            return asterisco;
+        }
+    }
+}

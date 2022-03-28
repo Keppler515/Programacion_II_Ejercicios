b94dd52292608f3f06815a3650f6c4bfa65a8251
Keppler515 <maxisosa25@hotmail.com>
2022-03-28 00:18:45 -0300

    Ejercicios I01 a I09

---
+++ EjercicioI09/LogicaTrianguloEquilatero/CalculaEquilatero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LogicaTrianguloEquilatero
+{
+    public class CalculaEquilatero
+    {
+        public static string asterisco = "*";
+        public static string espacio = " ";
+        public static string DibujarTrianguloEquilatero(int altura)
+        {
+            for (int i = 0; i < altura; i++)
+            {
+                for (int j = altura; j > i; j--)
+                {
+                    Console.Write($"{espacio}");
+                    espacio = espacio + " ";
+                }
+
+                Console.WriteLine($"{asterisco}");
+                asterisco = asterisco + "*";
+            }
+
+            return asterisco;
+        }
+
+    }
+}
---
+++ EjercicioI09/VistaTrianguloEquilatero/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LogicaTrianguloEquilatero;
+
+namespace VistaTrianguloEquilatero
+{
+    internal class Program
+    {
+        public static string stringAltura;
+        public static int altura;
+        public static string asterisco = "*";
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese una altura");
+            stringAltura = Console.ReadLine();
+            int.TryParse(stringAltura, out altura);
+
+            CalculaEquilatero.DibujarTrianguloEquilatero(altura);
+
+        }
+    }
+
+}

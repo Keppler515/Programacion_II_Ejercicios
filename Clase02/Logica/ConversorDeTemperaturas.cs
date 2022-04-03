f3f936615055225a9a633a5003b3f7733f16202a
Keppler515 <maxisosa25@hotmail.com>
2022-04-03 14:14:03 -0300

    Clase 02

---
+++ Clase02/Logica/ConversorDeTemperaturas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Logica
+{
+    public class ConversorDeTemperaturas // PascalCase
+    {
+       private const float ceroAbsoluto = 273.15f; //lowercase
+
+        public static float ConvertirCelciusAKelvin(float temperaturaCelcius) // el nombre del método en PascalCase
+        {
+            return temperaturaCelcius + ceroAbsoluto;
+        }
+
+        public static double ConvertirKelvinACelcius(int temperaturaKelvin)
+        {
+            return temperaturaKelvin - ceroAbsoluto;
+        }
+    }
+}

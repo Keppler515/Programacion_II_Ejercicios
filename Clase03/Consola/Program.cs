09efad0eb428b71d7b8bf15ae919f71fe4e22915
Keppler515 <maxisosa25@hotmail.com>
2022-04-03 14:15:19 -0300

    Clase 03

---
+++ Clase03/Consola/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Logica;
+using System.Text;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*
+            Mascota perro = new Mascota();//Esto es una instancia de la clase Mascota
+            perro.raza = "Calle";
+            perro.edad = 5;
+            perro.nombre = "Tobias";
+            perro.especie = "Perro";
+            perro.hambre = true;
+
+            Console.WriteLine(perro.Saludar());
+        */
+            Mascota gato = new Mascota("Kali", "gato", 1);
+
+
+
+            Mascota.Alimentar(gato);
+            Console.WriteLine(gato.Saludar());
+            Console.WriteLine($"Kali tiene hambre? {gato.hambre}");
+           // Console.WriteLine($"Tobias tiene hambre? {perro.hambre}");
+        }
+    }
+}
